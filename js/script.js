97f3c6589c132139dfb534cac9451ec756154916
Devonian P10 <54732787+Devonian10@users.noreply.github.com>
2024-08-12 22:25:05 +0800

    Proses pengerjaan hitungan dan desain

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
-var berat_badan = InputEvent
+
+function gender(laki_laki, perempuan){
+let laki_laki = document.getElementByid("laki-laki");
+let perempuan = document.getElementById("perempuan")
+ 
+    console.log
+}
+function BMI (berat_badan, umur,tinggi_badan){
+    let berat_badan = document.getElementById("berat_badan")
+    let tinggi_badan = document.getElementById("tinggi_badan")
+    let umur = document.getElementById("umur")
+
+    console.log(parseInt(berat_badan)/ parseInt(tinggi_badan)**2);
+}
+let 
 const BMI = berat_badan / (tinggi_badan)**2;
+
+
 
